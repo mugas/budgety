ff60262ec36df0082fdcb6304115a62b893e1341
Ricardo Moreira <ricardo.d.moreira.rm@gmail.com>
2018-10-29 23:38:19 +0200

    add function Constructor to the budget controller

--- app.js
+++ app.js
@@ -1,5 +1,59 @@
 //Budget Controller
-var budgetController = (function() {})();
+var budgetController = (function() {
+  var Expense = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var Income = function(id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var data = {
+    allItems: {
+      exp: [],
+      inc: []
+    },
+    totals: {
+      exp: 0,
+      inc: 0
+    }
+  };
+
+  return {
+    addItem: function(type, des, val) {
+      var newItem, ID;
+
+      //Create new ID
+      if (data.allItems[type].length > 0) {
+        ID = data.allItems[type][data.allItems[type].length - 1].id + 1;
+      } else {
+        ID = 0;
+      }
+
+      //Create new item based on "inc" or "exp" type
+      if (type === "exp") {
+        newItem = new Expense(ID, des, val);
+      } else if (type === "inc") {
+        newItem = new Income(ID, des, val);
+      }
+
+      //Push into data structure
+      data.allItems[type].push(newItem);
+      //from the array allItems get the type of the function and
+      // accordingly if it's a expense or income add a new item
+
+      //return the new element
+      return newItem;
+    },
+    testing: function() {
+      console.log(data);
+    }
+  };
+})();
 
 // UI Controller
 var UIController = (function() {
@@ -45,9 +99,11 @@ var controller = (function(budgetCtrl, UICtrl) {
   };
 
   var ctrlAddItem = function() {
+    var input, newItem;
     // 1. Get the filled input data
-    var input = UICtrl.getInput();
+    input = UICtrl.getInput();
     //2. Add the item to the budget controller
+    newItem = budgetCtrl.addItem(input.type, input.description, input.value);
     //3. Add new item to UI
     //4. Calculate the budget
     //5. Display the budget on the UI
