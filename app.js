905bf060d69433ce63f00008a35bdf99773ec88a
Ricardo Moreira <ricardo.d.moreira.rm@gmail.com>
2018-10-30 23:42:08 +0200

    starting add html strings to the UI controller

--- app.js
+++ app.js
@@ -74,6 +74,22 @@ var UIController = (function() {
         value: document.querySelector(DOMstrings.inputValue).value
       };
     },
+    addListItem: function(obj, type) {
+      var html;
+      //create HTML string with placeholder text
+      if (type === "inc") {
+        html =
+          '<div class="item clearfix" id="income-0"> <div class="item__description">Salary</div><div class="right clearfix"><div class="item__value">+ 2,100.00</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      } else if (type === "exp") {
+        html =
+          '<div class="item clearfix" id="expense-0"><div class="item__description">Apartment rent</div><div class="right clearfix"><div class="item__value">- 900.00</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      }
+
+      //replace the placeholder with some actual data
+
+      //insert the html into the dom
+    },
+
     getDOMstrings: function() {
       return DOMstrings;
     }
