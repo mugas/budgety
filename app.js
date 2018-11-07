24415813dcbfed4fdbc8076f78f046889fb321ab
Ricardo Moreira <ricardo.d.moreira.rm@gmail.com>
2018-11-07 20:16:47 +0200

    start event delegation for the delete button

--- app.js
+++ app.js
@@ -102,7 +102,8 @@ var UIController = (function() {
     budgetLabel: ".budget__value",
     incomeLabel: ".budget__income--value",
     expensesLabel: ".budget__expenses--value",
-    percentageLabel: ".budget__expenses--percentage"
+    percentageLabel: ".budget__expenses--percentage",
+    container: ".container"
   };
   return {
     //This functions are accessible by all modules
@@ -184,6 +185,9 @@ var controller = (function(budgetCtrl, UICtrl) {
         ctrlAddItem();
       }
     });
+    document
+      .querySelector(DOM.container)
+      .addEventListener("click", ctrlDeleteItem);
   };
 
   var updateBudget = function() {
@@ -214,6 +218,12 @@ var controller = (function(budgetCtrl, UICtrl) {
       //5 Calculate and Update budget
       updateBudget();
     }
+  };
+
+  var ctrlDeleteItem = function(event) {
+    var itemID;
+
+    itemID = event.target.parentNode.parentNode.parentNode.parentNode.id;
   };
 
   return {
